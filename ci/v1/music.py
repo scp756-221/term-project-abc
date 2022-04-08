6cb920bf9886bdaab38a55519fa0382db646f3f0
root <zla236@sfu.ca>
2022-04-07 20:41:01 -0700

    add class playlist and functions

--- ci/v1/music.py
+++ ci/v1/music.py
@@ -98,3 +98,76 @@ class Music():
             self._url + m_id,
             headers={'Authorization': self._auth}
         )
+
+
+class PlayList():
+    """Python API for the music service.
+
+    Handles the details of formatting HTTP requests and decoding
+    the results.
+
+    Parameters
+    ----------
+    url: string
+        The URL for accessing the music service. Often
+        'http://cmpt756s2:30001/'. Note the trailing slash.
+    auth: string
+        Authorization code to pass to the music service. For many
+        implementations, the code is required but its content is
+        ignored.
+    """
+    def __init__(self, url, auth):
+        self._url = url
+        self._auth = auth
+
+    def create(self, pl_name, song):
+        """Create an artist, song pair.
+
+        Parameters
+        ----------
+        artist: string
+            The artist performing song.
+        song: string
+            The name of the song.
+
+        Returns
+        -------
+        (number, string)
+            The number is the HTTP status code returned by Music.
+            The string is the UUID of this song in the music database.
+        """
+        r = requests.post(
+            self._url,
+            json={'Artist': pl_name,
+                  'SongTitle': song},
+            headers={'Authorization': self._auth}
+        )
+        return r.status_code, r.json()['playlist_id']
+
+    def add(self, pl_id, music_id):
+        r = requests.put(
+            self._url + 'add_to_list/' + pl_id,
+            json={"music_id" : music_id},
+            headers={'Authorization': self._auth}
+            )
+        return r.status_code
+
+    def delete(self, pl_id, music_id):
+        """Delete an artist, song pair.
+
+        Parameters
+        ----------
+        m_id: string
+            The UUID of this song in the music database.
+
+        Returns
+        -------
+        Does not return anything. The music delete operation
+        always returns 200, HTTP success.
+        """
+        r = requests.put(
+            self._url + 'delete_from_list/' + pl_id,
+            json={'music_id' : music_id},
+            headers={'Authorization': self._auth}
+        )
+        return r.status_code
