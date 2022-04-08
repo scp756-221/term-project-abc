a9334673c732ce9fc978d1c3b27ff601e442666c
root <zla236@sfu.ca>
2022-04-07 20:49:38 -0700

    add class playlist and functions

--- ci/v1/playlist.py
+++ ci/v1/playlist.py
@@ -1,5 +1,5 @@
 """
-Python  API for the music service.
+Python  API for the playlist service.
 """
 
 # Standard library modules
@@ -8,8 +8,8 @@ Python  API for the music service.
 import requests
 
 
-class Music():
-    """Python API for the music service.
+class PlayList():
+    """Python API for the playlist service.
 
     Handles the details of formatting HTTP requests and decoding
     the results.
@@ -28,7 +28,7 @@ class Music():
         self._url = url
         self._auth = auth
 
-    def create(self, artist, song):
+    def create(self, pl_name, song):
         """Create an artist, song pair.
 
         Parameters
@@ -46,14 +46,42 @@ class Music():
         """
         r = requests.post(
             self._url,
-            json={'Artist': artist,
+            json={'Artist': pl_name,
                   'SongTitle': song},
             headers={'Authorization': self._auth}
         )
-        return r.status_code, r.json()['music_id']
+        return r.status_code, r.json()['playlist_id']
 
-    def read(self, m_id):
-        """Read an artist, song pair.
+    def add(self, pl_id, music_id):
+        r = requests.put(
+            self._url + 'add_to_list/' + pl_id,
+            json={"music_id" : music_id},
+            headers={'Authorization': self._auth}
+            )
+        return r.status_code
+
+    def delete_song(self, pl_id, music_id):
+        """Delete an artist, song pair.
+
+        Parameters
+        ----------
+        m_id: string
+            The UUID of this song in the music database.
+
+        Returns
+        -------
+        Does not return anything. The music delete operation
+        always returns 200, HTTP success.
+        """
+        r = requests.put(
+            self._url + 'delete_from_list/' + pl_id,
+            json={'music_id' : music_id},
+            headers={'Authorization': self._auth}
+        )
+        return r.status_code
+    
+    def read(self, pl_id):
+        """Read an playlist
 
         Parameters
         ----------
@@ -72,17 +100,17 @@ class Music():
           If status is not 200, None.
         """
         r = requests.get(
-            self._url + m_id,
+            self._url + pl_id,
             headers={'Authorization': self._auth}
             )
         if r.status_code != 200:
             return r.status_code, None, None
 
         item = r.json()['Items'][0]
-        return r.status_code, item['Artist'], item['SongTitle']
+        return r.status_code, item['listname'], item['SongTitle']
 
-    def delete(self, m_id):
-        """Delete an artist, song pair.
+    def delete(self, pl_id):
+        """Delete an playlist
 
         Parameters
         ----------
@@ -95,7 +123,6 @@ class Music():
         always returns 200, HTTP success.
         """
         requests.delete(
-            self._url + m_id,
+            self._url + pl_id,
             headers={'Authorization': self._auth}
         )
-        
